f314c9e8994ba4e65edbeebac78381a2e92f4a0c
Mia McGrow <158192165+miamcgrow@users.noreply.github.com>
2024-03-07 10:06:17 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@ namespace CMP1903_A1_2324
     {
         static void Main(string[] args)
         {
-            Game game = new Game(); /// Creates a new game object
-            game.dice_Display(); /// Calls the display dice method
-
             Testing test = new Testing(); /// Creates a new test object
             test.test_Game(); /// Calls the test game method
             test.test_Die(); /// Calls the test die method 
+            
+            Game game = new Game(); /// Creates a new game object
+            game.dice_Display(); /// Calls the display dice method
         }
     }
 }
